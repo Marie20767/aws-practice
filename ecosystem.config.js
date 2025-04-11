74052c9fc0ab52521f5625491dacd57ee1941eac
Marie <marie@embedsignage.com>
2025-04-11 15:51:58 +0300

    Added ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: "aws-practice",
+      watch: true,
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
+      script: "build/index.js",
+      env: {
+        AWS_REGION: process.env.AWS_REGION,
+        ACCESS_KEY: process.env.ACCESS_KEY,
+        SECRET_ACCESS_KEY: process.env_SECRET_ACCESS_KEY,
+        S3_BUCKET_NAME: process.env.S3_BUCKET_NAME,
+      },
+    },
+  ],
+};
