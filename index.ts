d2399c4e71328d84f0caa78cbaa5dc39878e2707
Marie <marie@embedsignage.com>
2025-04-11 15:38:23 +0300

    Added --env flag in start:pm2 script

--- index.ts
+++ index.ts
@@ -11,7 +11,6 @@ import { saveImage } from 'utils';
 import { Readable } from 'stream';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
-
 const { S3_BUCKET_NAME, AWS_REGION } = process.env;
 
 const app = express();
