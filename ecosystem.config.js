add2c4d17d7614f612b0597bbcae51871008414d
Marie <marie@embedsignage.com>
2025-04-11 16:01:27 +0300

    Updated ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,12 +5,7 @@ module.exports = {
       watch: true,
       log_date_format: "YYYY-MM-DD HH:mm:ss",
       script: "build/index.js",
-      env: {
-        AWS_REGION: process.env.AWS_REGION,
-        ACCESS_KEY: process.env.ACCESS_KEY,
-        SECRET_ACCESS_KEY: process.env_SECRET_ACCESS_KEY,
-        S3_BUCKET_NAME: process.env.S3_BUCKET_NAME,
-      },
+      env_file: ".env"
     },
   ],
 };
